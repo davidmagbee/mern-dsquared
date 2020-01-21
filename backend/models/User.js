3ef1c0dd326b839ca9b43df22546befdef323adc
David Loesch <davidloesch1@gmail.com>
2020-01-21 11:15:19 -0800

    updated User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,24 @@ const mongoose = require('../db/connection');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email: String,
+    name: {
+        firstName: String,
+        lastName: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    register_date: {
+        type: Date,
+        default: Date.now
+    },
     tasks: [
         {
             type: Schema.Types.ObjectId,
