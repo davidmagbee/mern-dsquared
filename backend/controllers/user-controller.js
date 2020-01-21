b7bf8023139c1eab9acc9c84859992ed6f291fda
David Loesch <davidloesch1@gmail.com>
2020-01-21 11:56:28 -0800

    updating user controller

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,10 +1,44 @@
 const express = require("express")
 const router = express.Router()
+const bcrypt = require("bcryptjs")
 
 const User = require("../models/User")
 const Task = require("../models/Task")
 
 router.post("/", (req, res) => {
+    const { name, email, password } = req.body
+
+    if(!email || !password){
+        return(res.status(400).json({msg: "please fill out form"}))
+    }
+
+    User.findOne({ email: email })
+        .then(user => {
+            if(user){
+                return res.status(400).json({msg: "User already exists"})
+            }
+
+            const newUser = new User({
+                email,
+                password
+            })
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if(err) throw err
+                    newUser.password = hash
+                    newUser.save()
+                        .then(user => {
+                            res.json({
+                                user: {
+                                    id: user._id,
+                                    email: user.email
+                                }
+                            })
+                        })
+                })
+            }
+        })
     res.send("register")
 })
 
