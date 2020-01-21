61c3919458e4825a8992f67f6cf74380079cbeea
David Loesch <davidloesch1@gmail.com>
2020-01-21 13:47:21 -0800

    completed mvp backend

--- backend/controllers/task-controller.js
+++ backend/controllers/task-controller.js
@@ -9,32 +9,33 @@ router.get("/", (req, res) => {
 })
 
 router.post("/", (req, res) => {
-    User.find({ _id: req.body.user._id }).then(user => {
-        Task.create(req.body.task.title).then(task => {
-            user[0].tasks.push(task._id)
-            task.user = user._id
+    // User.find({ _id: req.body.user._id }).then(user => {
+        Task.create(req.body).then(task => {
+            // user[0].tasks.push(task._id)
+            // task.user = user._id
 
-            user[0].save()
-            task.save()
+            // user[0].save()
+            // task.save()
 
-            res.json(user)
+            // res.json(user)
+            res.json(task)
         })
     })
-})
+// })
 
 router.put("/", (req, res) => {
-    User.find({ _id: req.body.user._id }).then(user => {
-        Task.findOneAndUpdate({ _id: req.body.task._id }, req.body.task, { new: true }).then(task => {
-            user[0].save()
-            task.save()
+    // User.find({ _id: req.body.user._id }).then(user => {
+        Task.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }).then(task => {
+            // user[0].save()
+            // task.save()
 
-            res.json(user)
+            res.json(task)
         })
     })
-})
+// })
 
 router.delete("/", (req, res) => {
-    Task.findOneAndDelete({ _id: req.body.user._id }).then(task => res.json(task))
+    Task.findOneAndDelete({ _id: req.body._id }).then(task => res.json(task))
 })
 
 module.exports = router
