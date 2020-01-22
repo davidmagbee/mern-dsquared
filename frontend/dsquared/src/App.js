3bd41f35a65881da96d0f69b26c32423a8e38e9d
David Loesch <davidloesch1@gmail.com>
2020-01-22 10:12:12 -0800

    added the get feature

--- frontend/dsquared/src/App.js
+++ frontend/dsquared/src/App.js
@@ -1,92 +1,110 @@
-import React, { Component } from 'react';
-import ToDoForm from './components/ToDoForm'
-import ToDoList from './components/ToDoList'
-import './App.css';
-const axios = require('axios').default
+import React, { Component } from "react";
+import ToDoForm from "./components/ToDoForm";
+import ToDoList from "./components/ToDoList";
+import "./App.css";
+const axios = require("axios").default;
 
-
-class App extends Component { 
-  constructor(){
-    super()
-    this.state={
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
       toDo: [],
-      value: ''
-    }
-    this.deleteTask = this.deleteTask.bind(this)
-    this.markThrough = this.markThrough.bind(this)
+      value: ""
+    };
+    this.deleteTask = this.deleteTask.bind(this);
+    this.markThrough = this.markThrough.bind(this);
   }
 
-
-
-  //this adds each new task to the this.state.toDo array 
+  //this adds each new task to the this.state.toDo array
   //This will need to be the post request
-  addString = (e) => {
-    e.preventDefault()
+  addString = e => {
+    e.preventDefault();
     let task = {
       title: this.state.value,
       complete: false
-    }
-    let newArray = this.state.toDo.slice()
-    newArray.push(task)
+    };
+    let newArray = this.state.toDo.slice();
+    newArray.push(task);
     this.setState({
       toDo: newArray,
-      value: ''
+      value: ""
+    });
+  };
+
+  setToDo = (array) => {
+    this.setState({
+      toDo: array
     })
   }
 
-  //this simply holds the value in the form 
-  handleChange = (e) => {
+  //this simply holds the value in the form
+  handleChange = e => {
     this.setState({
       value: e.target.value
-    })
-  }
+    });
+  };
 
   //this pulls the deleted task from the toDo array
-  //this will need to be delete 
-  deleteTask = (e) => {
-    let copyArray = this.state.toDo.slice(0)
-    copyArray.splice(e, 1)
+  //this will need to be delete
+  deleteTask = e => {
+    let copyArray = this.state.toDo.slice(0);
+    copyArray.splice(e, 1);
     this.setState({
       toDo: copyArray
-    })
-  }
+    });
+  };
 
   //this marks through selected task
   //this will need to be for a put request to change "completed" to true
-  markThrough = (e) => {
-    let paragraph = e.target.nextSibling
-    if(paragraph.classList.contains("mark-off")) {
-      paragraph.classList.remove("mark-off")
+  markThrough = e => {
+    let paragraph = e.target.nextSibling;
+    if (paragraph.classList.contains("mark-off")) {
+      paragraph.classList.remove("mark-off");
     } else {
-      paragraph.classList += " mark-off"   
+      paragraph.classList += " mark-off";
     }
-    if(e.target.classList.contains("fa-square")) {
-      e.target.classList.remove("fa-square")
-      e.target.classList += " fa-check-square"
+    if (e.target.classList.contains("fa-square")) {
+      e.target.classList.remove("fa-square");
+      e.target.classList += " fa-check-square";
     } else {
-      e.target.classList += " fa-square"
+      e.target.classList += " fa-square";
     }
-  }
+  };
 
   render() {
-
+    
     //on load, pull all the tasks from database and place in this.state.toDo
-    axios.get('http://localhost:5000/task').then(res => {
-      console.log(res.data)
-    })
+    axios
+      .get("http://localhost:5000/task")
+      .then(res => {
+        let array = [];  
+        res.data.map(el => {
+          let obj = {
+            title: el.title,
+            complete: el.complete
+          };
+          array.push(obj);
+        });
+        return array
+      }).then(array => {
+        this.setToDo(array)
+      })
 
-    return(
+    return (
       <div>
-        <ToDoForm {...this.state} add={this.addString} change={this.handleChange} />
-        <ToDoList 
-          list={this.state.toDo} 
-          delete={this.deleteTask} 
+        <ToDoForm
+          {...this.state}
+          add={this.addString}
+          change={this.handleChange}
+        />
+        <ToDoList
+          list={this.state.toDo}
+          delete={this.deleteTask}
           mark={this.markThrough}
-        /> 
-
+        />
       </div>
-    )
+    );
   }
 }
 
-export default App;	
+export default App;
